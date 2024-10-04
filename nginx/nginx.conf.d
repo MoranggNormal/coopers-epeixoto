40dbc180b3beec1e3a91f4b97743d5535c1073b3
Euller Peixoto <epeixoto.dev@gmail.com>
2024-10-04 20:38:15 -0300

    setup nginx

---
+++ nginx/nginx.conf.d
@@ -0,0 +1,18 @@
+upstream client {
+    hash $request_uri consistent;
+    server client:3000;
+}
+
+server {
+
+    listen 80;
+    server_name client;
+
+    location / {
+        proxy_pass http://client;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+        add_header X-Cache-Status $upstream_cache_status always;
+        proxy_redirect off;
+    }
+}
