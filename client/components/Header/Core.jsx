d2b99bac2c52615ba4b5b22b8fe5ebdfdea56795
Euller Peixoto <epeixoto.dev@gmail.com>
2024-10-04 21:28:48 -0300

    initial header component

---
+++ client/components/Header/Core.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import logo from "@/app/icons/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="w-full absolute top-[54px] px-[24px] md:px-[60px] lg:px-[80px]">
+      <div className="flex justify-between">
+        <div>
+          <Image
+            src={logo.src}
+            alt="Coopers Digital Production"
+            width={180}
+            height={50}
+            priority
+          />
+        </div>
+        <div className="">
+          <button className="bg-black w-[120px] h-[40px] text-[14px] leading-[21px] font-bold text-white">
+            Sign in
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
