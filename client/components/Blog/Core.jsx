ac32e05c3ec8860d1dc01135d455f245299b7a40
Euller Peixoto <epeixoto.dev@gmail.com>
2024-10-05 19:04:19 -0300

    adjust carousel items

--- client/components/Blog/Core.jsx
+++ client/components/Blog/Core.jsx
@@ -21,12 +21,15 @@ const breakpoints = {
   1024: {
     slidesPerView: 3,
   },
+  1440: {
+    slidesPerView: 4,
+  },
 };
 
 const Blog = () => {
   return (
-    <section className="mt-20 mb-80 w-full flex  lg:ml-[140px] justify-center lg:justify-start">
-      <div className="relative w-[320px] sm:w-[640px] md:w-[800px] lg:w-[1080px] xl:w-[1300px] h-[520px] rounded-[10px] bg-primary pl-[34px] pt-10 pb-10 lg:pl-20 lg:pt-20 lg:pb-20">
+    <section className="mt-20 mb-80 w-full flex lg:ml-[140px] justify-center lg:justify-start">
+      <div className="relative w-[320px] sm:w-[640px] md:w-[740px] lg:w-[800px] xl:w-[1300px] h-[520px] rounded-[10px] bg-primary pl-[34px] pt-10 pb-10 lg:pl-20 lg:pt-20 lg:pb-20">
         <h2 className="font-bold text-[32px] lg:text-[48px] text-white">
           good things
         </h2>
