dc41d17a4035f77617f155accfac7e46666cc0fb
Euller Peixoto <epeixoto.dev@gmail.com>
2024-10-06 06:41:06 -0300

    rename route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,26 +5,23 @@ const { register } = require("../controllers/authController");
 
 const router = express.Router();
 
-router
-  .route("/")
-  // .get(auth)
-  .post(
-    [
-      check("name", "Name is required").not().isEmpty(),
-      check("email", "Please include a valid email").isEmail(),
-      check(
-        "password",
-        "Please enter a password with 6 or more characters"
-      ).isLength({ min: 6 }),
-    ],
-    (req, res, next) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      next();
-    },
-    register
-  );
+router.route("/register").post(
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  register
+);
 
 module.exports = router;
