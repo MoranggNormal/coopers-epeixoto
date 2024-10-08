55ae8c28bfcd26b5e8ca7213ed360806bccc3480
Euller Peixoto <epeixoto.dev@gmail.com>
2024-10-08 20:29:10 -0300

    use blog post images from figma

--- client/mock/posts.js
+++ client/mock/posts.js
@@ -1,31 +1,43 @@
+import post1 from "@/static/images/post1.png";
+import post2 from "@/static/images/post2.png";
+import post3 from "@/static/images/post3.png";
+
 export const posts = [
   {
     id: 1,
     context: "function",
     title: "Organize your daily job enhance your life performance",
-    imageSrc:
-      "https://img.freepik.com/fotos-gratis/chama-brilhante-abstrata-cai-em-ia-geradora-de-iluminacao-eletrica_188544-8092.jpg?w=740&t=st=1728141659~exp=1728142259~hmac=5dd61324d44b71083405f713eb00f01418b20606df0f15f7d0461ee7f59932f2",
+    imageSrc: post1.src,
+  },
+  {
+    id: 2,
+    context: "function",
+    title: "Mark one activity as done makes your brain understands the power of doing.",
+    imageSrc:post2.src
   },
   {
     id: 3,
     context: "function",
-    title: "lorem daily job enhance your life performance",
-    imageSrc:
-      "https://img.freepik.com/vetores-gratis/fundo-de-aquarela-rosa-pintado-a-mao-detalhado_1048-17039.jpg?t=st=1728143751~exp=1728147351~hmac=6c5e1c2aa93367ecd6b80d6b6e03f9a7fe144b52f25df0296cc8a4f80d5d3449&w=1380",
+    title: "Careful with missunderstanding the difference between a list of things and a list of desires.",
+    imageSrc: post3.src
   },
-  {
-    id: 2,
-    context: "test",
-    title: "Organize your life performance",
-    imageSrc:
-      "https://img.freepik.com/free-photo/astronaut-diving-ocean_23-2151549752.jpg?t=st=1728142963~exp=1728146563~hmac=6159952ad53253a0fb5112272887a969b9ff28d28255ce353b49b70e9a518fd8&w=740",
-  },
-
   {
     id: 4,
-    context: "test",
-    title: "Organize ormance",
-    imageSrc:
-      "https://img.freepik.com/premium-photo/surrealist-beach-collage-design_23-2151729738.jpg?w=740",
+    context: "function",
+    title: "Organize your daily job enhance your life performance",
+    imageSrc: post1.src,
   },
+  {
+    id: 5,
+    context: "function",
+    title: "Mark one activity as done makes your brain understands the power of doing.",
+    imageSrc:post2.src
+  },
+  {
+    id: 6,
+    context: "function",
+    title: "Careful with missunderstanding the difference between a list of things and a list of desires.",
+    imageSrc: post3.src
+  },
+  
 ];
